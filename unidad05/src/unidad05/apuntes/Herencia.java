ae13f0b273d748d4172da29196d059ac361682b3
vricheb973 <vricheb973@g.educaand.es>
2026-02-24 14:38:40 +0100

    Update Herencia.java

--- unidad05/src/unidad05/apuntes/Herencia.java
+++ unidad05/src/unidad05/apuntes/Herencia.java
@@ -1,5 +1,16 @@
 package unidad05.apuntes;
 
 public class Herencia {
+	
+	/*
+	 * Dibujar el diagrama de clases
+	 * Decidir qué clases son abstractas y cuáles no
+	 * Creo las clases respetando la herencia (con constructor y getters/setters)
+	 * Genero toString e equals supertipo
+	 * 	toString: Usamos el String.format obligatorio
+	 * 	equals: ponemos ifs del padre y modificamos la última línea
+	 * 
+	 * Pongo atención a los enumerados (hay que tratarlos como Strings).
+	 */
 
 }
